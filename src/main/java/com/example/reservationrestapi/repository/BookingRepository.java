d59a147821c1b595dbd77804bc34d94986151843
SamiraKetabifard <samiraketabi.fard@gmail.com>
2025-06-29 21:38:52 +0330

    add test

--- src/main/java/com/example/reservationrestapi/repository/BookingRepository.java
+++ src/main/java/com/example/reservationrestapi/repository/BookingRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository<Booking,Integer> {
 
-    Booking findBookingById(int id);
 }
--- src/main/java/com/example/reservationrestapi/repository/UserRepository.java
+++ src/main/java/com/example/reservationrestapi/repository/UserRepository.java
@@ -2,12 +2,9 @@ package com.example.reservationrestapi.repository;
 
 import com.example.reservationrestapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 
